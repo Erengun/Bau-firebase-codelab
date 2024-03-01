30cf627d0c3b51824ab50f205099e07d2143ac65
Eren GÜN <erengun4@gmail.com>
2024-03-01 18:36:04 +0300

    Add Firebase authentication and profile screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
+  final String? title;
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
+        title: const Text('Hello World'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      appBar: AppBar(
+                        title: const Text('Hi Bro'),
+                      ),
+                      actions: [
+                        SignedOutAction((context) {
+                          Navigator.pop(context);
+                        })
+                      ],
+                      children: [
+                        Image.asset('assets/flutterfire_300x.png'),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.person))
+        ],
       ),
       body: Center(
         child: Column(
           children: [
-            Image.asset('dash.png'),
+            Image.asset('assets/dash.png'),
             Text(
               'Welcome!',
               style: Theme.of(context).textTheme.displaySmall,
             ),
+            ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Sign Out'))
           ],
         ),
       ),
