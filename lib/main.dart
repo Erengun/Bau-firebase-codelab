62f3f70adad2ef017ef2b10debf74b096422b7a0
Eren GÜN <erengun4@gmail.com>
2024-03-01 18:35:59 +0300

    Add Firebase initialization and import Firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 // TODO(codelab user): Get API key
-const clientId = 'YOUR_CLIENT_ID';
+const clientId = 'AIzaSyBqLWsqFjYAdGyihKTahMRDQMo0N6NVjAs';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'Project-X',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
